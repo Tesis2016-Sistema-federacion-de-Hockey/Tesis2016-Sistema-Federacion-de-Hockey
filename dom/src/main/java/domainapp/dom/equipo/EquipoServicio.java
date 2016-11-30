b787b126462b4c2da7f3bbc67db0cbd5d546ea63
maikelekiam <maikelekiam@maikelekiam-pc>
2016-11-30 15:58:23 -0300

    ISSUE #73:Agrego reporte en PDF para imprimir planilla de partido

--- dom/src/main/java/domainapp/dom/equipo/EquipoServicio.java
+++ dom/src/main/java/domainapp/dom/equipo/EquipoServicio.java
@@ -266,7 +266,7 @@ public class EquipoServicio {
 			datasource.addParticipante(reporteListaBuenaFe);
 		}
 		
-		File file = new File("reporte105.jrxml");
+		File file = new File("listaBuenaFe.jrxml");
 		
 		FileInputStream input = null;
 		
--- dom/src/main/java/domainapp/dom/partido/PartidoServicio.java
+++ dom/src/main/java/domainapp/dom/partido/PartidoServicio.java
@@ -1,6 +1,12 @@
 package domainapp.dom.partido;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.isis.applib.annotation.Action;
 import org.apache.isis.applib.annotation.ActionLayout;
@@ -14,8 +20,19 @@ import org.apache.isis.applib.annotation.Where;
 import org.apache.isis.applib.query.QueryDefault;
 import org.apache.isis.applib.services.i18n.TranslatableString;
 import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.value.Blob;
 
 import domainapp.dom.equipo.Equipo;
+import domainapp.dom.jugador.Jugador;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 @DomainService(
         nature = NatureOfService.VIEW,
@@ -41,8 +58,114 @@ public class PartidoServicio {
     @MemberOrder(sequence = "1")
     public List<Partido> listarPartidosPorEquipo(Equipo equipo) {
     	return repositoryService.allMatches(new QueryDefault<Partido>(Partido.class, "listarPartidosPorEquipo", "equipo", equipo));
-    	
     }
+    
+    //PARA IMPRIMIR
+    @ActionLayout(named = "Imprimir Planilla de Partido")
+	public Blob imprimirPlanillaPartido(final Partido partido) throws JRException, IOException {
+		
+    	List<String> jugadoresLocal=new ArrayList<String>();
+    	for(Jugador jug : partido.getEquipoLocal().getListaBuenaFe()){
+    		jugadoresLocal.add(jug.getNombre()+" "+jug.getApellido());
+    	}
+    	
+//    	List<String> jugadoresVisitantes=new ArrayList<String>();
+//    	for(Jugador jug : partido.getEquipoVisitante().getListaBuenaFe()){
+//    		jugadoresVisitantes.add(jug.getNombre()+" "+jug.getApellido());
+//    	}
+    	
+    	File file = new File("planillaPartido.jrxml");
+		
+		FileInputStream input = null;
+		
+		try {
+			input = new FileInputStream(file);
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+    	
+		JasperDesign jd = JRXmlLoader.load(input);
+		
+		JasperReport reporte = JasperCompileManager.compileReport(jd);
+    	
+		Map<String, Object> parametros = new HashMap<String, Object>();
+		
+		parametros.put("jugadoresLocal", jugadoresLocal);
+		parametros.put("club", partido.getEquipoLocal().getNombre());
+		parametros.put("division", partido.getEquipoLocal().getDivision().getNombre());
+		parametros.put("equipoLocal", partido.getEquipoLocal().getNombre());
+		parametros.put("equipoVisitante", partido.getEquipoVisitante().getNombre());
+		parametros.put("torneo", partido.getEquipoLocal().getDivision().getTorneo().getNombre());
+//		parametros.put("horario", partido.getFechaHora());
+		
+		
+		
+		
+		
+//		parametros.put("jugadoresVisitantes", jugadoresVisitantes);
+		
+		JasperPrint jasperPrint = JasperFillManager.fillReport(reporte, parametros,new JREmptyDataSource());
+    	
+		JasperExportManager.exportReportToPdfFile(jasperPrint, "/tmp/salida.pdf");
+		
+		File archivo = new File("/tmp/salida.pdf");
+		
+		byte[] fileContent = new byte[(int) archivo.length()];
+		
+		if (!(archivo.exists())) {
+			try {
+				archivo.createNewFile();
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+		try {
+			FileInputStream fileInputStream = new FileInputStream(archivo);
+
+			fileInputStream.read(fileContent);
+			
+			fileInputStream.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			
+			return new Blob(partido.getNombre()+" - "+partido.getEquipoLocal().getNombre()+
+					" vs "+partido.getEquipoVisitante().getNombre()+".pdf", "application/pdf", fileContent);
+			
+		} catch (Exception e) {
+			byte[] result = new String("error en crear archivo").getBytes();
+			return new Blob("error.txt", "text/plain", result);
+		}
+	}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @javax.inject.Inject
+    Partido partido;
+    
     
     @javax.inject.Inject
     RepositoryService repositoryService;
